baafb46d9a57eb5e978011c57e710f1bce94eaaf
Alan Tresa Ananyase <3657165+alan-ananyase@users.noreply.github.com>
2019-09-22 21:23:46 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,88 @@
+# print(type(False))
+# for i in range(1, 10):
+# 	if i == 2:
+# 		print(False)
+# 	elif i == 4:
+# 		print('this is four')
+# 	else:
+# 		print(i)
+
+# print("Enter a number")
+# inp = int(input())
+# for num in range(1,inp+1):
+# 	print('*' * num)
+#Output: * ** *** ****
+
+# print("Enter a number")
+# inp = int(input())
+# for num in range(1, inp+1):
+# 	print(str(num)*num)
+#Output: 1 22 333 4444
+
+# print("Enter a number")
+# inp = int(input())
+# for num in range(1, inp+1):
+# 	if num % 2 > 0:
+# 		print (str(num)*num)
+# Output: 1 333 55555 7777777
+
+# print("Enter a Number")
+# inp = int(input())
+# for i in range(1,inp+1):
+# 	num = 1
+# 	for j in range(1, i+1):
+# 		print (num, end=" ")
+# 		num = num + 1
+# 	print('\r')
+# Output: 1 12 123 1234 12345
+
+# print("Enter number of rows")
+# inp = int(input())
+# num = 1
+# for i in range(1, inp+1):
+# 	for j in range(1, i+1):
+# 		print(num, end=" ")
+# 		num = num + 1
+# 	print('\r')
+# Output: 1 23 456 78910
+
+# print("Enter number of rows")
+# inp = int(input())
+# num = 1
+# for i in range(1, inp+1):
+# 	for j in range(1, i+1):
+# 		print(num, end=" ")
+# 		num = num + 2
+# 	print('\r')
+# Output: 1 35 7911 13151719
+
+# print("Enter number of rows")
+# inp = int(input())
+# num = 1
+# for i in range(1, inp+1):
+# 	if(num == 1):
+# 		print(num)
+# 		num = num + 1
+# 	else:
+# 		for j in range(1, i+1):
+# 			print(num, end=" ")
+# 			num = num + 2
+# 		print('\r')
+# Output: 1 24 6810 12141618
+
+# print("Enter number of rows")
+# inp = int(input())
+# num = 1
+# for i in range(1, inp+1):
+# 	print('*'*num)
+# 	num = num + 1
+# num = num - 1
+# for j in range(inp, 0, -2):
+# 	print('*'*num)
+# 	num = num - 1
+# arr = [[0,1,2],[3,4,5],[6,7,8]]
+# #print [8,
+# # print(isinstance(arr, list))
+# for i,v in enumerate(arr):
+# 	print(arr[i])
+
